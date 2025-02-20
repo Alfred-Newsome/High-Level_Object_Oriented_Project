2f0db96e283aae80acf0d3cd4b94d6298ace781f
Alfred-Newsome <117668010+Alfred-Newsome@users.noreply.github.com>
2025-02-20 18:29:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace High_level_Object_Oriented_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
